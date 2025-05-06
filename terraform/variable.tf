e55b811849e7464150ff0f07e0df82036d0c25f7
joel <joel.pereira@hdsofttech.com>
2025-05-06 12:37:42 +0530

    added terraform files

---
+++ terraform/variable.tf
@@ -0,0 +1,10 @@
+variable "aws_region" {
+  description = "AWS region to deploy the resources"
+  type        = string
+  default     = "us-east-1" # Change this if needed
+}
+
+variable "bucket_name" {
+  description = "Name of the S3 bucket to host the React app"
+  type        = string
+}
